43d9e06d2b1977bcec0fcfd93aaa37a782533946
Sazzad <netsazzad@gmail.com>
2021-06-28 01:53:45 +0600

    User mutation login changed

--- src/gql/mutations/userMutation.js
+++ src/gql/mutations/userMutation.js
@@ -4,6 +4,10 @@ const GraphQL = require("graphql");
 //const UserController = require("../../controllers/userController");
 
 const { GraphQLNonNull, GraphQLString } = GraphQL;
+
+const matchPassword = require("../../middlewares/matchPassword");
+const generateToken = require("../../middlewares/generateToken");
+
 //importing the user model
 const User = require("../../models/userModel");
 // lets import our user type
@@ -26,6 +30,23 @@ const login = () => {
     },
     async resolve(parent, args) {
       //return await UserController.login(fields);
+      const user = await User.find({ email });
+      if (user) {
+        const isValid = await matchPassword(password, user.password);
+        if (isValid) {
+          const token = await generateToken(user._id, user.email);
+          return {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            status: user.status,
+            role: user.role,
+            token,
+          };
+        } else {
+          throw new Error("Password is not valid");
+        }
+      }
       return await User.create(args);
     },
   };
